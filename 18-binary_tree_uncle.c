2cffe41c51f8791fccb1f425e0f7e53b3c1b30a9
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-11-03 01:29:00 +0000

    Yeah

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,21 +7,18 @@
  * Return: A pointer to the uncle node, or NULL if the node is NULL,
  * the parent is NULL, the grandparent is NULL, or the parent has no sibling.
  */
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *grandparent;
+	binary_tree_t *uncle;
 
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-		return (NULL); /* If the node is NULL, return NULL */
+	if (!node || !node->parent || !node->parent->parent
+		|| !node->parent->parent->left
+		|| !node->parent->parent->right)
+		return (0);
 
-    grandparent = node->parent->parent;
+	uncle = node->parent->parent->right;
+	uncle = (node->parent == uncle) ? node->parent->parent->left : uncle;
 
-	if (grandparent->left == node->parent && grandparent->right != NULL)
-		return (grandparent->right); /* Return right child of gp as the uncle */
-
-	if (grandparent->right == node->parent &&
-		grandparent->left != NULL)
-		return (grandparent->left); /* Return left child of gp as the uncle */
-
-	return (NULL);
+	return uncle;
 }
