2f321930f476d9ed6f9d216e0b5465f310dc60a0
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-11-03 01:20:28 +0000

    For now

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,10 +9,12 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *grandparent;
+
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL); /* If the node is NULL, return NULL */
 
-	binary_tree_t *grandparent = node->parent->parent;
+    grandparent = node->parent->parent;
 
 	if (grandparent->left == node->parent && grandparent->right != NULL)
 		return (grandparent->right); /* Return right child of gp as the uncle */
