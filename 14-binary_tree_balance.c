ee94aed126a448d6a88a83e4f6f7619dd06825c0
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-11-03 01:07:58 +0000

    BNJDSO

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,13 +9,17 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height;
+	int right_height;
+	int balance_factor;
+
 	if (tree == NULL)
 		return (0);
 
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	int balance_factor = left_height - right_height;
+	balance_factor = left_height - right_height;
 
 	return (balance_factor);
 }
