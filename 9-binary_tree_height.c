d128b210b5a58c2aa859dfabcc5bc0db7ab016aa
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-11-03 00:53:19 +0000

    Nine

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,16 +6,26 @@
  *
  * Return: The height of the tree, or 0 if the tree is empty (NULL).
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+
+	size_t Lheight;
+	size_t Rheight;
+
+
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	{
 		return (0);
-
-	size_t left_height = binary_tree_height(tree->left) + 1;
-	size_t right_height = binary_tree_height(tree->right) + 1;
-
-	if (left_height > right_height)
-		return (left_height);
+	}
 	else
-		return (right_height);
+	{
+		Lheight = binary_tree_height(tree->left) + 1;
+		Rheight = binary_tree_height(tree->right) + 1;
+
+		if (Lheight > Rheight)
+			return (Lheight);
+		else
+			return (Rheight);
+	}
 }
